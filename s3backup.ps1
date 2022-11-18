ea5be796c6720a3733a7931161259ed52fcf2e8a
Kevin Burkeland <kevin@burke.land>
2022-11-18 13:40:57 -0800

    added comments

--- s3backup.ps1
+++ s3backup.ps1
@@ -1,14 +1,23 @@
+#your s3 bucket name
 $bucketName = "mybucket"
+#makes time date variables
 $bDate = Get-Date -Format "yyyy-MM-dd"
 $bTime = Get-Date -Format "HH-mm"
+#makes sure the backups file exists
 if (!(Test-Path -Path "$HOME\backups")) {
     New-Item -ItemType Directory -Path "$HOME\backups"
 }
+#iterates through the directories in backupdirs.txt
 Get-Content .\backupdirs.txt | ForEach-Object {
+    #gets the name of the directory
     $dir = $_
     $dirName = Split-Path $dir -Leaf
+    #zips the directory
     Compress-Archive -Path $dir -DestinationPath "$HOME\backups\$bDate-$bTime-$dirName.zip"
+    #fills variables with the file name and path
     $file = "$HOME\backups\$bDate-$bTime-$dirName.zip"
+    #uploads the file to s3
     Write-S3Object -BucketName $bucketName -File $file -Key "backups\$bDate-$bTime-$dirName.zip"
+    #cleans up the file
     Remove-Item $file
 }
--- s3backup.sh
+++ s3backup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+#your s3 bucket name
 bucketName="mybucket"
+#time date variables
 bDate=$(date +%Y%m%d)
 bTime=$(date +%H%M)
+#makes sure the backups directory exists
 if [ ! -d "~/backups" ]; then
     mkdir ~/backups
 fi
+#iterates through the directories in the backupdirs.txt file
 for dir in `cat ./backupdirs.txt`
 do
+    #fills variable with the directory name
     dirName=$(echo $dir |rev|cut -d/ -f1|rev)
+    #creates a tar.gz file of the directory
     tar zcvf ~/backups/$bDate-$bTime-$dirName-backup.tar.gz $dir
+    #uploads the tar.gz file to s3
     aws s3 cp ~/backups/$bDate-$bTime-$dirName-backup.tar.gz s3://$bucketName/backups/
+    #cleans up the tar.gz file
     rm ~/backups/$bDate-$bTime-$dirName-backup.tar.gz
 done
